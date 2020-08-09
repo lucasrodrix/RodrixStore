8c797378c48696cdf1cc84c1e7ef48c98c3a071a
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-08-09 01:24:37 -0300

    Created components and created routes with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Default from './components/Default';
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Product from './components/Product';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">column number one</div>
-        <div className="col-6"><span><i className="fa fa-home"></i></span></div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={ProductList}></Route>
+        <Route path='/details' component={Details}></Route>
+        <Route path='/cart' component={Cart}></Route>
+        <Route component={Default}></Route>
+      </Switch>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Default.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Default(){
+    return(
+        <div>
+            <h3><strong>404</strong> Page Not Found</h3>
+        </div>
+    );
+}
+
+export default Default;
