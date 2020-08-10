edb8b5132dea3433584dc03ac997f5afb5b51296
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-08-10 11:05:26 -0300

    create links to navigate in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
+
+
 
 function Navbar(){
     return(
         <div>
-            <h3>Hello from Navbar</h3>
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                {/*https://www.iconfinder.com/icons/1243689/call_phone_icon
+                    Creative Commons (Attribution 3.0 Unported);
+                    https://www.iconfinder.com/Makoto_msk */}
+                <Link to='/'>
+                    <img src={logo} alt="store" className="navbar-brand"/>
+                </Link>
+                <ul className="navber-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to='/' className='nav-link'>
+                            Products
+                        </Link>
+                    </li>
+                </ul>
+                <Link to='/cart' className='ml-auto'>
+                    <button><i className="fa fa-cart-plus">MyCart</i></button>
+                </Link>
+            </nav>
         </div>
     );
 }
