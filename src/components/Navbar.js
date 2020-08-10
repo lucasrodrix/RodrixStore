91963f302983b88ae24bfb0b718df92958a7e13b
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-08-10 11:54:47 -0300

    created Button component and added style in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
+import styled from 'styled-components';
+import ButtonContainer from './Button';
 
 
 
 function Navbar(){
     return(
         <div>
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 {/*https://www.iconfinder.com/icons/1243689/call_phone_icon
                     Creative Commons (Attribution 3.0 Unported);
                     https://www.iconfinder.com/Makoto_msk */}
                 <Link to='/'>
                     <img src={logo} alt="store" className="navbar-brand"/>
                 </Link>
-                <ul className="navber-nav align-items-center">
+                <ul className="navbar-nav align-items-center">
                     <li className="nav-item ml-5">
                         <Link to='/' className='nav-link'>
-                            Products
+                            products
                         </Link>
                     </li>
                 </ul>
                 <Link to='/cart' className='ml-auto'>
-                    <button><i className="fa fa-cart-plus">MyCart</i></button>
+                    <ButtonContainer>
+                        <span className='mr-2'>
+                            <i className="fa fa-cart-plus">MyCart</i>
+                        </span>
+                    </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         </div>
     );
 }
+
+const NavWrapper = styled.nav`
+    background: var(--mainBlue);
+    .nav-link{
+        color: var(--mainWhite)!important;
+        font-size: 1.3rem;
+        text-transform: capitalize !important;
+    }
+`;
 
 export default Navbar;
